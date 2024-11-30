5a86f4af2d594068613109efe036e23807ed4a87
Tom Schaepsmeier <tom.schaepsmeier@gmail.com>
2024-11-30 14:55:24 +0100

    added initialization of TTClient in main file

---
+++ ttexport.cpp
@@ -0,0 +1,13 @@
+#include "tt-client/TTClient.h"
+
+int main() {
+
+    // get all data from TimeTree
+    TTClient ttClient = TTClient("", "");
+
+    // store data properly
+
+    // convert data into different format
+
+    return 0;
+}
